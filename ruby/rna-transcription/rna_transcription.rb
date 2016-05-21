c7efbd57393ccdaca45871ae74ccfd96328cd543
Constantijn Schepens <constantijnschepens@gmail.com>
2016-05-22 00:00:49 +0100

    Second solution that gets rid of symbols

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -2,15 +2,15 @@ class Complement
   VERSION=3
 
   COMPLEMENT_TABLE = {
-    G: "C",
-    C: "G",
-    T: "A",
-    A: "U"
+    "G" => "C",
+    "C" => "G",
+    "T" => "A",
+    "A" => "U"
   }
   def self.of_dna(dna_nucleotides)
     rna_nucleotides = dna_nucleotides.chars.map do |dna_nucleotide|
-      raise ArgumentError unless COMPLEMENT_TABLE.keys.include? dna_nucleotide.to_sym
-      COMPLEMENT_TABLE[dna_nucleotide.to_sym]
+      raise ArgumentError unless COMPLEMENT_TABLE.keys.include? dna_nucleotide
+      COMPLEMENT_TABLE[dna_nucleotide]
     end
     rna_nucleotides.join
   end
