8eb3f417895568e43c93e0c58e161b32a209b2d0
Constantijn Schepens <constantijnschepens@gmail.com>
2016-05-30 21:08:52 +0100

    Much better fully recursive solution to ruby sieve

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -7,17 +7,20 @@ class Sieve
 
   def primes
     return [] unless remaining_numbers.count > 0
-    next_prime = remaining_numbers[0]
-    while next_prime < remaining_numbers.last do
-      remove_multiple(next_prime, next_prime)
-      index = remaining_numbers.find_index next_prime
-      next_prime = remaining_numbers[index + 1]
-    end
+    handle_next_prime(remaining_numbers[0])
 
     remaining_numbers
   end
 
   private
+    def handle_next_prime(number)
+      return unless number < remaining_numbers.last
+      remove_multiple(number, number)
+
+      index = remaining_numbers.find_index number
+      handle_next_prime(remaining_numbers[index + 1])
+    end
+
     def remove_multiple(number, increment)
       number_to_delete = number + increment
       remaining_numbers.delete(number_to_delete)
