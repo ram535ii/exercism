1a994f388724d598bb2f0d7805256966793dd898
Constantijn Schepens <constantijnschepens@gmail.com>
2016-05-30 18:12:32 +0100

    Slightly cleaner solution to ruby/difference_of_squares

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -3,20 +3,21 @@ class BookKeeping
 end
 
 class Squares
+  attr_reader :numbers
+
   def initialize(top_natural_number)
-    @top_natural_number = top_natural_number
+    @numbers = top_natural_number == 0 ? [0] : (1..top_natural_number)
   end
 
   def square_of_sum
-    (1..@top_natural_number).reduce(:+)**2
+    numbers.reduce(:+)**2
   end
 
   def sum_of_squares
-    (1..@top_natural_number).inject { |sum, number| sum + number**2 }
+    numbers.inject { |sum, number| sum + number**2 }
   end
 
   def difference
-    return 0 if @top_natural_number == 0
     square_of_sum - sum_of_squares
   end
 end
