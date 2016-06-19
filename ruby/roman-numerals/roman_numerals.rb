088dcc0fb8508b5d8101b0b93292fb4d76120cf8
Constantijn Schepens <constantijnschepens@gmail.com>
2016-06-19 11:09:51 +0100

    ruby#roman_numerals minor cleanup

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -21,30 +21,31 @@ class Fixnum
     }
   }
   def to_roman
-    # digits = self.length
     result = ""
+
     digits.reverse.each_with_index do |val, index|
       next if val == 0
+      numerals = VALUES[index]
       if val == 1
-        roman_digit = VALUES[index][1]
+        roman_digit = numerals[1]
       elsif val == 2
-        roman_digit = VALUES[index][1] + VALUES[index][1]
+        roman_digit = numerals[1] + numerals[1]
       elsif val == 3
-        roman_digit = VALUES[index][1] + VALUES[index][1] + VALUES[index][1]
+        roman_digit = numerals[1] + numerals[1] + numerals[1]
       elsif val == 4
-        roman_digit = VALUES[index][1] + VALUES[index][5]
+        roman_digit = numerals[1] + numerals[5]
       elsif val == 5
-        roman_digit = VALUES[index][5]
+        roman_digit = numerals[5]
       elsif val == 6
-        roman_digit = VALUES[index][5] + VALUES[index][1]
+        roman_digit = numerals[5] + numerals[1]
       elsif val == 7
-        roman_digit = VALUES[index][5] + VALUES[index][1] + VALUES[index][1]
+        roman_digit = numerals[5] + numerals[1] + numerals[1]
       elsif val == 8
-        roman_digit = VALUES[index][5] + VALUES[index][1] + VALUES[index][1] + VALUES[index][1]
+        roman_digit = numerals[5] + numerals[1] + numerals[1] + numerals[1]
       elsif val == 9
-        roman_digit = VALUES[index][1] + VALUES[index][10]
+        roman_digit = numerals[1] + numerals[10]
       elsif val == 10
-        roman_digit = VALUES[index][10]
+        roman_digit = numerals[10]
       end
       result = "#{roman_digit}#{result}"
     end
