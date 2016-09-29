3240da3c1c47fa1ad27a400aff2353e11f631455
Constantijn Schepens <constantijnschepens@gmail.com>
2016-09-29 19:10:09 +0100

    Move generator to module and handle uniqueness of name

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,27 +1,48 @@
-class Robot
+module UniqueNameGenerator
+  @@used_names = []
 
-  def initialize
-    set_random_name
+  def self.unique_name
+    name = random_name
+    while @@used_names.include?(name) do
+      name = random_name
+    end
+
+    @@used_names << name
+    name
   end
 
-  def name
-    @name
+  def self.random_name
+    "#{two_random_letters}#{three_random_integers}"
   end
 
-  def reset
-    set_random_name
+  def self.return_name(name)
+    @@used_names.delete(name)
   end
 
-  def set_random_name
-    @name = "#{two_random_letters}#{three_random_integers}"
-  end
-
-  def two_random_letters
+  private_class_method def self.two_random_letters
     ("A".."Z").to_a.sample(2).join
   end
 
-  def three_random_integers
+  private_class_method def self.three_random_integers
     (0..9).to_a.sample(3).join
+  end
+end
+
+class Robot
+  attr_reader :name
+
+  def initialize(generator: UniqueNameGenerator)
+    @generator = UniqueNameGenerator
+    set_name
+  end
+
+  def reset
+    @generator.return_name(@name)
+    set_name
+  end
+
+  def set_name
+    @name = @generator.unique_name
   end
 end
 
