169d8f912f5a9374b55c103d182ebb4dfc9d97ad
Constantijn Schepens <constantijnschepens@gmail.com>
2016-10-26 20:37:32 +0100

    scala: hamming, don't use return

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,8 +1,9 @@
 object Hamming {
   def compute(strand_one: String, strand_two: String) : Option[Int] = {
-    if(strand_one.length != strand_two.length) return None
-
-    Some(strand_one.zip(strand_two).count { case (elem_one, elem_two) => elem_one != elem_two})
+    if(strand_one.length != strand_two.length)
+      None
+    else
+      Some(strand_one.zip(strand_two).count { case (elem_one, elem_two) => elem_one != elem_two})
   }
 }
 
