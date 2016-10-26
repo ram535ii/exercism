203602eb737ed04f7a7d08d6bfbca5cf744257b5
Constantijn Schepens <constantijnschepens@gmail.com>
2016-10-26 20:35:13 +0100

    scala word count, properly functional solution

--- scala/word-count/src/main/scala/word_count.scala
+++ scala/word-count/src/main/scala/word_count.scala
@@ -1,9 +1,8 @@
 class Phrase(string: String) {
   def wordCount() : Map[String,Int] = {
-    val m = collection.mutable.Map[String, Int]().withDefaultValue(0)
-    string.split(" ").foreach { word =>
-      m.update(word, m(word) + 1)
-    }
-    return m.toMap
+    string.toLowerCase
+          .split("[^\\w']+")
+          .groupBy(word => word)
+          .mapValues(_.length)
   }
 }
