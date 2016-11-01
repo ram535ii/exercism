4a5e973b14c1f05a1d9ae0436948a83fedb473e3
Constantijn Schepens <constantijnschepens@gmail.com>
2016-11-01 20:43:22 +0000

    scala: wordcount minor improvement - identity

--- scala/word-count/src/main/scala/word_count.scala
+++ scala/word-count/src/main/scala/word_count.scala
@@ -2,7 +2,7 @@ class Phrase(string: String) {
   def wordCount() : Map[String,Int] = {
     string.toLowerCase
           .split("[^\\w']+")
-          .groupBy(word => word)
+          .groupBy(identity)
           .mapValues(_.length)
   }
 }
