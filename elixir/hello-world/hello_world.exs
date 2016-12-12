6d031facb1148e9385f3aea1b1a31ec807dfc174
Constantijn Schepens <constantijnschepens@gmail.com>
2016-12-12 15:27:46 +0000

    Improve elixir/hello-world

--- elixir/hello-world/hello_world.exs
+++ elixir/hello-world/hello_world.exs
@@ -3,9 +3,8 @@ defmodule HelloWorld do
   Greets the user by name, or by saying "Hello, World!"
   if no name is given.
   """
+  @spec hello(String.t) :: String.t
   def hello(name \\ "World") do
-
-    "Hello, " <> name <> "!"
-
+    "Hello, #{name}!"
   end
 end
