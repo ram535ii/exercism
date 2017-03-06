ed52b4e69902cefb289af95ca574e2d0300346d6
Constantijn Schepens <constantijnschepens@gmail.com>
2017-03-06 13:54:10 +0000

    ruby/word-count slightly more readable

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -5,13 +5,16 @@ class Phrase
 
   def word_count
     word_count_per_word = {}
+
     @sentence.split(/[^a-zA-Z0-9']/)
               .reject(&:empty?)
               .map(&:downcase)
               .map { |word| word.gsub(/\A'|'\z/, "") }
               .each do |word|
-                word_count_per_word[word] ? word_count_per_word[word] += 1 : word_count_per_word[word] = 1
+                word_count_per_word[word] ||= 0
+                word_count_per_word[word] += 1
               end
+
     word_count_per_word
   end
 end
