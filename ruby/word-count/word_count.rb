a6befb5741a73b00493915b6458c660a1798b133
Constantijn Schepens <constantijnschepens@gmail.com>
2017-03-06 13:53:09 +0000

    ruby/word-count first solution

---
+++ ruby/word-count/word_count.rb
@@ -0,0 +1,21 @@
+class Phrase
+  def initialize(sentence)
+    @sentence = sentence
+  end
+
+  def word_count
+    word_count_per_word = {}
+    @sentence.split(/[^a-zA-Z0-9']/)
+              .reject(&:empty?)
+              .map(&:downcase)
+              .map { |word| word.gsub(/\A'|'\z/, "") }
+              .each do |word|
+                word_count_per_word[word] ? word_count_per_word[word] += 1 : word_count_per_word[word] = 1
+              end
+    word_count_per_word
+  end
+end
+
+module BookKeeping
+  VERSION = 1
+end
