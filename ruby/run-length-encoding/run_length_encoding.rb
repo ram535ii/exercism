fc8cd6c64ebbfb75dbf47fa7f57f5aa7550d6a6f
Constantijn Schepens <constantijnschepens@gmail.com>
2017-03-06 17:10:43 +0000

    ruby/run-length-encoding gsub is incredible, based on soln by bonyii

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,27 +1,13 @@
-module RunLengthEncoding
+class RunLengthEncoding
   def self.encode(input)
-    input
-      .scan(/((.)(\2*))/)
-      .map do |full_str, char|
-        if full_str.length == 1
-          char
-        else
-          full_str.length.to_s + char
-        end
-      end
-      .join
+    input.gsub(/(.)(\1+)/) { ($2 + $1).length.to_s + $1 }
   end
 
   def self.decode(input)
-    input
-      .scan(/(\d*)(\D)/)
-      .map do |count, char|
-        char * (count.to_i > 0 ? count.to_i : 1)
-      end
-      .join
+    input.gsub(/(\d+)(.)/) { $2 * $1.to_i }
   end
 end
 
 module BookKeeping
-  VERSION=2
+  VERSION = 2
 end
