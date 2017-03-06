13cac1eced177706648b3463c9fcd9bba36a53b1
Constantijn Schepens <constantijnschepens@gmail.com>
2017-03-06 16:57:45 +0000

    ruby/run-length-encoding damn regexs are powerful, now this is really clean

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,45 +1,26 @@
 module RunLengthEncoding
   def self.encode(input)
-    output = ""
-    previous_char = input.slice!(0)
-    count = 1
-
-    input.chars.each_with_index do |char, index|
-      if char == previous_char
-        count+=1
-      else
-        addition = count == 1 ? "#{previous_char}" : "#{count}#{previous_char}"
-        output += addition
-        count = 1
+    input
+      .scan(/((.)(\2*))/)
+      .map do |full_str, char|
+        if full_str.length == 1
+          char
+        else
+          full_str.length.to_s + char
+        end
       end
-
-      previous_char = char
-    end
-
-    addition = count == 1 ? "#{previous_char}" : "#{count}#{previous_char}"
-    output += addition
-
-    output
+      .join
   end
 
   def self.decode(input)
-    output = ""
-    segments = input.scan(/\d+|\D+/)
-
-    until segments.length == 0
-      segment = segments.shift
-      if segment.to_i == 0
-        output += segment
-      else
-        following_chars = segments.shift
-        output += "#{following_chars.slice!(0) * segment.to_i}#{following_chars.empty? ? '' : following_chars}"
+    input
+      .scan(/(\d*)(\D)/)
+      .map do |count, char|
+        char * (count.to_i > 0 ? count.to_i : 1)
       end
-    end
-
-    output
+      .join
   end
 end
-
 
 module BookKeeping
   VERSION=2
