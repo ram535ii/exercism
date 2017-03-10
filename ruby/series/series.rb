5c824a5d87e2c0e83a39cd01af35e818f92a97c8
Constantijn Schepens <constantijnschepens@gmail.com>
2017-03-10 12:34:43 +0000

    ruby/series - minor efficiency improvement

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -8,7 +8,7 @@ class Series
     raise ArgumentError.new("n can't be greater than the string length") unless @whole_string_length >= n
 
     max_substring_start_index = @whole_string_length - n
-    (0..max_substring_start_index).to_a.map do |start_index|
+    (0..max_substring_start_index).map do |start_index|
       @whole_string.slice(start_index, n)
     end
   end
