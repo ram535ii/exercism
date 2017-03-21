40ff80e9423159673d5ffa67e93caa99ac850de6
Constantijn Schepens <constantijnschepens@gmail.com>
2017-03-21 21:56:27 +0000

    ruby/prime_factors fast version of first soln

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,13 +1,16 @@
+require 'prime'
+
 module PrimeFactors
   def self.for(number)
-    result = []
-
-    while(number > 1) do
-      divisor = (2..number).find { |num| number % num == 0 }
-      result << divisor
-      number /= divisor
-    end
-
-    result
+    return [] if number == 1
+    recursive_for(number)
   end
+
+  def self.recursive_for(number, acc=[])
+    return (acc << number) if Prime.prime?(number)
+
+    divisor = (2..number).find { |num| number % num == 0 }
+    return recursive_for(number/divisor, acc << divisor)
+  end
+  private_class_method :recursive_for
 end
