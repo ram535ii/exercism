d17a3052888e0169503dbae5d9ca04e6df853814
Constantijn Schepens <constantijnschepens@gmail.com>
2024-08-16 11:55:33 +0100

    Better soln

--- python/ghost-gobble-arcade-game/arcade_game.py
+++ python/ghost-gobble-arcade-game/arcade_game.py
@@ -44,4 +44,4 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     :return: bool - has the player won the game?
     """
 
-    return has_eaten_all_dots and (not touching_ghost or power_pellet_active)
+    return has_eaten_all_dots and not lose(power_pellet_active, touching_ghost)
